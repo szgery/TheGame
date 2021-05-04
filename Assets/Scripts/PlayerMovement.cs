bcdf6495f58644a45f1c67127bd5129d53421ba1
Szergely <szabogergely805@gmai.com>
2021-05-04 18:10:39 +0200

    Fények javítása

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
-    {
+    {        
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
