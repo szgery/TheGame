fb95142e524c1bf52a28e83f4a2931de21a0941d
szabodani04 <danielmark0401@gmail.com>
2021-05-04 18:47:44 +0200

    újra az egészet

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -50f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
