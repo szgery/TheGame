5ba0b7c6f85ee7055a6f262f76a66007486ea3b3
szgery <szabogergely805@gmail.com>
2021-05-08 20:48:45 +0200

    Mozgatható ágy, javított padló, meghosszabbított folyosó

---
+++ Assets/Scripts/MoveBedScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBedScript : MonoBehaviour
+{
+    public bool isTrigger;
+
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (isTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (isTrigger)
+        {
+            GUI.Box(new Rect(0, 0, 200, 25), "Nyomj E-t, hogy arrébb lökd");
+        }
+    }
+}
